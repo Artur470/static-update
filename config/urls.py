2a883d61b46b0e1cf15e3654d3c34431dbecf3a9
Artur <arturunkulov30@gmail.com>
2024-09-20 20:57:20 +0600

    update code srt

--- config/urls.py
+++ config/urls.py
@@ -1,40 +1,44 @@
 from django.contrib import admin
-from django.urls import path
 from django.urls import path, include
-from config import settings
-from django.conf.urls.static import static
-from django.urls import re_path
+from django.http import HttpResponse  # Добавляем импорт для HttpResponse
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 
+# Определяем функцию для корневого URL
+def index(request):
+    return HttpResponse("Welcome to your Django app!")
+
+
+# Настройка Swagger
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-
-
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
-
+# Список маршрутов
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #apps
+
+    # Корневой маршрут
+    path('', index, name='home'),
+
+    # Маршруты ваших приложений
     path('product/', include('product.urls')),
     path('users/', include('users.urls')),
     path('cart/', include('cart.urls')),
 
-    #swagger
+    # Swagger
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-
 ]
